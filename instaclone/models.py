e884e517962e26d2ac8392b39f2da9b52305d302
SamFriedland <sam@friedland.org.uk>
2020-07-20 21:23:58 +0100

    commit

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,15 +1,25 @@
 from django.db import models
 
 # Create your models here.
-def#
+
 class User(models.Model):
-    username = models.CharField(max_length=30)
+    username = models.CharField(max_length=64)
+    email
+    date_of_birth
+    profile_pic
+    bio
 
-class Followers(models.Model):
-    followers = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.username
 
-class Following(models.Model):
-    following = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Follows(models.Model):
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followed")
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    def __str__(self):
+        return ("f' {self.User} is followed by {following]")
+
+
 class Instapost(models.Model):
     post_title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -18,13 +28,11 @@ class Instapost(models.Model):
     def __str__(self):
         return self.post_title
 
-
-
 class Like(models.Model):
     post = models.ForeignKey(Instapost, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-class Comment(models.Model):d
+class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Instapost, on_delete=models.CASCADE)
     comment_text = models.CharField(max_length=200)
@@ -33,5 +41,4 @@ class Comment(models.Model):d
     def __str__(self):
         return self.comment_text
 
-
-
+#cd /d C:/Users/sam/PycharmProjects/InstaClone
